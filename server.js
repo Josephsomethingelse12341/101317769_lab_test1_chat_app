11a92d876408cba3bf433fb0770fbec63db39fcc
Josephsomethingelse12341 <joseph.degois17@gmail.com>
2023-02-07 00:48:25 -0500

    installed socket.io

---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require("express")
+var mongoose = require("mongoose")
+var socket = require("socket.io")
+
+var app = express()
+
+var PORT = 8088
+
+var server = app.listen(PORT, () => {
+    console.log("Socket Server running at 8088: http://localhost:8088/")
+})
+
+//http://localhost:8088/
+app.get("/", (req, res) => {
+    res.sendFile( __dirname + "/login.html")
+})
+
+var io = socket(server)
+
+io.on("connection", (client) => {
+    console.log("Client Connection request")
+    console.log(client.id)
+
+    client.emit("welcome", "Welcome to Socket.io Chat Server")
+
+    client.on("hello", (data) => {
+        console.log(data)
+
+        const notice = {
+            status: false,
+            message: "Message to all"
+        }
+        
+        client.broadcast.emit("notice", notice)
+    })
+
+   
+    client.on("disconnect", () => {
+        console.log(`Client disconnected : ${client.id}`)
+    })
+})
+
+mongoose.connect('mongodb+srv://Josephadmin:7HYpxGTzZiOnEocy@cluster0.gsshbsu.mongodb.net/lab_test_1?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(success => {
+  console.log('Success Mongodb connection')
+}).catch(err => {
+  console.log('Error Mongodb connection')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
